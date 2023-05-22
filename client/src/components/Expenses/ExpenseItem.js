0595e0229b3257962e097ec8ed24051a6a4a69ef
FerdyLazuardi <ferdy.lazuardi2000@gmail.com>
2023-05-22 17:28:49 +0700

    styling

--- client/src/components/Expenses/ExpenseItem.js
+++ client/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,41 @@
 import React, { useState } from "react";
-import { FaEye, FaEdit } from "react-icons/fa";
+import { FaEye, FaEdit, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  console.log(props);
+  const styles = `
+  .btn {
+    padding: 10px;
+    border-radius:4px;
+  }
 
+  .btn-delete{
+    border: 1px solid rgba(250, 44, 90, 1);
+    background-color:white;
+    color: rgba(250, 44, 90, 1);
+  }
+
+  .btn-delete:hover{
+    border: 1px solid rgba(250, 44, 90, 1);
+    background-color:rgba(250, 44, 90, 1);
+    color:white
+  }
+  .trash-icon{
+    style={{ color: "rgba(250, 44, 90, 1)" }}
+  }
+  .btn-delete:hover .trash-icon {
+    color: #ffffff;
+  }
+
+  `;
   const [isDeleted, setIsDeleted] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const deleteHandler = async () => {
     try {
@@ -25,30 +51,77 @@ const ExpenseItem = (props) => {
     }
   };
 
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
   if (isDeleted) {
     return null; // Menghilangkan komponen setelah penghapusan berhasil dilakukan .
   }
 
   return (
     <>
-      <img src={props.image} style={{ width: "50%" }} />
-      <ExpenseDate date={props.datee} />
-
+      <style>{styles}</style>
+      <img src={props.image} style={{ width: "100%" }} />
       <div className="">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="">${props.amount}</div>
+        <div className="row mb-2">
+          <div className="col-md-6">
+            <button
+              className="btn btn-delete"
+              onClick={deleteHandler}
+              style={{ width: "100%" }}
+            >
+              <FaTrash className="trash-icon" /> Delete
+            </button>
+          </div>
+          <div className="col-md-6">
+            <Link to={`/update/${props.id}`}>
+              <button className="btn btn-edit" style={{ width: "100%" }}>
+                <FaEdit /> Edit
+              </button>
+            </Link>
+          </div>
+        </div>
+
         <Link to={`/details/${props.id}`}>
-          <button>
-            <FaEye />
-          </button>
-        </Link>
-        <button onClick={deleteHandler}>Delete</button>
-        <Link to={`/update/${props.id}`}>
-          <button>
-            <FaEdit />
-          </button>
+          <div className="d-grid">
+            <button className="btn btn-detail">
+              <FaEye /> Detail Product
+            </button>
+          </div>
         </Link>
       </div>
+
+      <Modal show={showModal} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src="assets/images/dlt.jpg"
+            alt="background"
+            className="img-fluid mt-2 mb-3 mx-auto d-block"
+            style={{ width: "30%" }}
+          />
+
+          <p className="text-center">
+            Are you sure you want to delete this product?
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            variant="danger"
+            onClick={deleteHandler}
+            className="delete-button"
+          >
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
